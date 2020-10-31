3f9ca1ea036641ff216d3af1797056d0c815c216
Amanuel333 <girmayamanuel23@gmail.com>
2020-10-31 07:56:26 -0700

    messing around

---
+++ src/test/java/Halloween.java
@@ -0,0 +1,15 @@
+public class Halloween {
+
+    public static void main(String[] args) {
+
+        String ss = "asdfghjk";
+
+        if(ss.startsWith("a")){
+            System.out.println("fuck");
+        }else {
+            System.out.println("damn");
+        }
+
+    }
+
+}
