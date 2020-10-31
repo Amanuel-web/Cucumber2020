f60676c928aace9bec2cbea55988c949883c9a1d
Amanuel333 <girmayamanuel23@gmail.com>
2020-10-31 13:28:23 -0700

    dude

--- src/test/java/Halloween.java
+++ src/test/java/Halloween.java
@@ -3,7 +3,7 @@ public class Halloween {
     public static void main(String[] args) {
 
         String ss = "asdfghjk";
-
+// add
         for (int i = 0; i <= ss.length()-1; i++){
 
             System.out.println(ss.charAt(i));
