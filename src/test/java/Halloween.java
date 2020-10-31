72d3df4883c3ac81838b505163863db81fbf2389
Amanuel333 <girmayamanuel23@gmail.com>
2020-10-31 09:43:48 -0700

    pls work

--- src/test/java/Halloween.java
+++ src/test/java/Halloween.java
@@ -4,15 +4,7 @@ public class Halloween {
 
         String ss = "asdfghjk";
 
-        if(ss.startsWith("b")){
-            System.out.println("fuck");
-        }else {
-            System.out.println("damn");
-        }
 
-        System.out.println("pppppsssssnnnn");
-
-        System.out.println("Why is it not working fuck");
 
 
 
